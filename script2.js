300568bbb670fe4341e951141b260975b7978a82
Malessi5 <72767353+Malessi5@users.noreply.github.com>
2021-04-19 12:15:15 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,217 @@
+const rollBtn = document.getElementById("roll-btn");
+const startBtn = document.getElementById("start-btn");
+const die1 = document.getElementById("one");
+const die2 = document.getElementById("two");
+const die3 = document.getElementById("three");
+const banner = document.getElementById("banner");
+const scoreBanner = document.getElementById("current-score");
+const p1Score = document.getElementById("player-one-score");
+const p2Score = document.getElementById("player-two-score");
+
+let p1Turn = true;
+let currentPlayer;
+const player1 = ["Player One",0];
+const player2 = ["Player Two",0];
+
+startBtn.addEventListener("click", startGame);
+
+function startGame(){
+    rollBtn.addEventListener("click", handleClick);
+    plTurn = true;
+    clearDice();
+    console.log("New Game");
+    console.log("Player One's turn to roll")
+    banner.textContent = "Player One's turn to roll";
+    scoreReset();
+    rollBtn.disabled = false;
+}
+
+function handleClick(){
+    if (p1Turn === true) {
+        currentPlayer = player1;
+        
+    } else {
+        currentPlayer = player2;
+    }
+   
+    let roll = diceRoll();
+    check456(roll,currentPlayer[0]);
+    check123(roll,currentPlayer[0]);
+    reroll(roll,currentPlayer[0]);
+
+    currentPlayer[1] = checkScore(roll,currentPlayer[0]); 
+        console.log(currentPlayer[1]);
+
+    if (p1Turn == true){
+        checkWinner(player1[1],player2[1]);
+    } 
+
+}
+
+function scoreReset(){
+    player1[1] = 0;
+    player2[1] = 0;
+}
+
+function checkWinner(score1,score2){
+    //compare scores to find the winner
+    if (score1 > score2){
+        banner.textContent = "Player One wins!"
+        console.log("Player One wins!");
+        rollBtn.disabled = true;
+
+    } else if (score1<score2) {
+        banner.textContent = "Player Two wins!"
+        console.log("Player Two wins!");
+        rollBtn.disabled = true;
+    } else if (score1 == score2){
+        banner.textContent = "It's a tie!"
+        console.log("It's a tie!");
+        rollBtn.disabled = true;
+    }
+    scoreReset();
+    
+}
+
+function diceRoll() {
+    let thisRoll = [];    
+    //generate 3 random numbers as dice rolls
+    for (let i = 1; i <= 3; i++) {
+           thisRoll.push(Math.floor(Math.random() * 6) +1);
+       };
+    showRoll(thisRoll);
+    //sort numbers in ascending order
+    thisRoll.sort(function(a, b){return a - b});
+    console.log(thisRoll);
+    return thisRoll;
+};
+
+function showRoll(roll){
+    //take the current roll and show the proper dice image
+    die1.src = diceImg(roll[0]);
+    die2.src = diceImg(roll[1]);
+    die3.src = diceImg(roll[2]);
+}
+
+function showScore(score){
+    if (p1Turn == true) {
+        if (score > 6){
+            score = score/3;
+            p1Score.textContent = "Triple" + score + "'s";
+        } else{
+        p1Score.textContent = score;    
+        }
+        
+    } else {
+        if (score > 6){
+            score = score/3;
+            p2Score.textContent = "Triple" + score + "'s";}
+            else{
+            p2Score.textContent = score;
+            }
+    }
+}
+
+function clearDice(){
+    die1.src = "images/dice1.png";
+    die2.src = "images/dice1.png";
+    die3.src = "images/dice1.png";
+    banner.textContent = "";
+    scoreBanner.textContent = "";
+    p1Score.textContent = "";
+    p2Score.textContent = "";
+}
+
+function check456(roll,currentPlayer){
+if (roll[0] === 4 && roll[1] === 5 && roll[2] === 6){
+    //player wins
+    console.log (currentPlayer + " Automatic win!");
+    banner.textContent = currentPlayer + " Automatic win!";
+    //end game
+    startGame();
+    };
+};
+
+function check123(roll,player){
+    if  (roll[0] === 1 && roll[1] === 2 && roll[2] === 3){
+        //player loses
+        console.log (player + " Automatic loss!");
+        banner.textContent = currentPlayer + " Automatic loss!";
+        //end game
+        startGame();
+    };
+};
+
+function reroll(roll,player){
+    if (roll[0] !== roll[1] && roll[1] !== roll[2]){
+        console.log("Roll again "+player);
+        scoreBanner.textContent = "Roll again "+ player;
+       // handleClick();
+    }
+}
+
+function checkScore(roll,player){
+    if (roll[0] === roll[1] && roll[1] === roll[2]) {
+        //player scores triple
+        console.log(player + " has triple " + roll[0]+"s");
+        scoreBanner.textContent = player + " rolled triple " + roll[0]+"s";
+        showScore(roll[0]);
+        swapTurns();
+        return roll[0] * 3;
+       
+    } else if (roll[0] === roll[1]){
+        //player rolls a pair 
+        console.log(player + " has a "+ roll[2]);
+        scoreBanner.textContent = player + " rolled a "+ roll[2];
+        showScore(roll[2]);
+        swapTurns();
+        return roll[2];
+
+    } else if (roll[1] === roll[2]){
+        //player rolls a pair 
+        console.log(player + " has a "+ roll[0]);
+        scoreBanner.textContent = player + " rolled a "+ roll[0];
+        showScore(roll[0]);
+        swapTurns();
+        return roll[0];
+
+    } 
+}
+
+function swapTurns() {
+    p1Turn = !p1Turn;
+    if (p1Turn == false){
+        banner.textContent = "Player Two's turn to roll";
+    console.log("Player Two's turn to roll")
+    }
+}
+
+function diceImg(num){
+    //pull the proper dice image based on the roll
+        switch (num) {
+            case 1:
+            return "images/dice1.png"
+            break;
+
+            case 2:
+            return "images/dice2.png"
+            break;
+
+            case 3:
+            return "images/dice3.png"
+            break;
+
+            case 4:
+            return "images/dice4.png"
+            break;
+
+            case 5:
+            return "images/dice5.png"
+            break;
+
+            case 6:
+            return "images/dice6.png"
+            break;
+        }
+    
+}
